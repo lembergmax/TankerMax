c5a6e589dcf8b90a0f33e3fc33c770fe25b9e908
Max Lemberg <151883998+lembergmax@users.noreply.github.com>
2026-06-07 20:57:14 +0200

    initial commit

---
+++ src/main/java/de/lembergmax/tankermax/TankerMaxApplication.java
@@ -0,0 +1,13 @@
+package de.lembergmax.tankermax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TankerMaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TankerMaxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/lembergmax/tankermax/TankerMaxApplicationTests.java
@@ -0,0 +1,13 @@
+package de.lembergmax.tankermax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TankerMaxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
